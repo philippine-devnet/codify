75d2224fe8bde904c3f5bcba3dda0511494507d1
Gian Santillan <lighthouse.baguio@gmail.com>
2013-06-05 05:41:56 +0800

    forked

--- app/config/database.php
+++ app/config/database.php
@@ -54,11 +54,11 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'pitg_activities',
-			'username'  => 'root',
-			'password'  => '',
-			'charset'   => 'utf8',
+			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'database',
+			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
+			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
+			'charset'   => 'utf8', 
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
 		),
